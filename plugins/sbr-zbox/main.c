e60eb0beadc3a8500351cf56d3aaae04a1f86abd
Anastasios Andronidis <anastasis90@yahoo.gr>
2020-10-08 05:56:27 +0100

    Net: Add demo network support

--- plugins/sbr-zbox/main.c
+++ plugins/sbr-zbox/main.c
@@ -15,9 +15,12 @@
 #undef basename
 
 #include <assert.h>
+#include <errno.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
 #include <sys/sysmacros.h>
@@ -25,7 +28,6 @@
 #include <time.h>
 #include <unistd.h>
 
-#include <assert.h>
 #include <zbox.h>
 
 static zbox_repo repo;
@@ -153,6 +155,114 @@ int ifstat(int fd, struct stat *statbuf) {
   return real_syscall(SYS_fstat, fd, (long)statbuf, 0, 0, 0, 0);
 }
 
+static int sbrsocket = -1;
+static int childlistensocket = -1;
+static int childacceptsocket = -1;
+
+#define CHILD_ACCEPT_PORT 2321
+
+// TODO: Should we accept more than 1 socket? How will we handle it?
+int isocket(int domain, int type, int protocol) {
+  // TODO: SOCK_NONBLOCK, SOCK_CLOEXEC
+  assert(domain == AF_INET || domain == AF_INET6);
+  assert(type == SOCK_STREAM || type == SOCK_SEQPACKET);
+  assert((type & SOCK_NONBLOCK) == 0);
+
+  return childlistensocket;
+}
+
+// TODO: We should ideally keep track of optval between setsockopt and get.
+// int igetsockopt(int sockfd, int level, int optname, void *optval,
+//                 socklen_t *optlen) {
+//   return 0;
+// }
+
+int isetsockopt(int sockfd, int level, int optname, const void *optval,
+                socklen_t optlen) {
+  return 0;
+}
+
+int ibind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+  return 0;
+}
+
+int ilisten(int sockfd, int backlog) { return 0; }
+
+int iaccept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
+  if (childacceptsocket >= 0) {
+    errno = ECONNRESET;
+    return -1;
+  }
+
+  // Initialize a sockaddr_in for the peer
+  struct sockaddr_in peer_addr = {0};
+
+  // Set the contents in the peer's sock_addr.
+  // Make sure the contents will simulate a real client that connects with the
+  // intercepted server, as the server may depend on the contents to make
+  // further decisions. The followings set-up should be fine with Nginx.
+  peer_addr.sin_family = AF_INET;
+  peer_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  peer_addr.sin_port = htons(CHILD_ACCEPT_PORT);
+
+  // copy the initialized peer_addr back to the original sockaddr. Note the
+  // space for the original sockaddr, namely addr, has already been allocated
+  if (addr && addrlen) {
+    memcpy(addr, &peer_addr, sizeof(peer_addr));
+    *addrlen = sizeof(peer_addr);
+  }
+
+  childacceptsocket = dup(childlistensocket);
+  return childacceptsocket;
+}
+
+int iaccept4(int sockfd, struct sockaddr *addr, socklen_t *addrlen, int flags) {
+  return iaccept(sockfd, addr, addrlen);
+}
+
+int igetsockname(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
+  struct sockaddr_in target;
+  socklen_t copylen = sizeof(target);
+
+  if (!addr || !addrlen)
+    return -1;
+
+  if (*addrlen < sizeof(target))
+    copylen = *addrlen;
+
+  target.sin_family = AF_INET;
+  target.sin_addr.s_addr = htonl(INADDR_ANY);
+  target.sin_port = htons(CHILD_ACCEPT_PORT);
+
+  memcpy(addr, &target, copylen);
+  *addrlen = copylen;
+
+  return 0;
+}
+
+ssize_t isendto(int sockfd, const void *buf, size_t len, int flags,
+                const struct sockaddr *dest_addr, socklen_t addrlen) {
+  assert(sockfd == childacceptsocket);
+
+  ssize_t rc = sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+
+  // char rbuf[1024] = {0};
+  // recv(sbrsocket, rbuf, 1024, 0); // sys: recvfrom
+  // printf("sbr: %s", rbuf);
+
+  return rc;
+}
+
+ssize_t irecvfrom(int sockfd, void *buf, size_t len, int flags,
+                  struct sockaddr *src_addr, socklen_t *addrlen) {
+  assert(sockfd == childacceptsocket);
+
+  // char msg[] = "lalalalalo\r\n";
+  // send(sbrsocket, msg, sizeof(msg), 0);
+
+  return recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
+}
+
 long handle_syscall(long sc_no, long arg1, long arg2, long arg3, long arg4,
                     long arg5, long arg6, void *wrapper_sp) {
   if (sc_no == SYS_clone && arg2 != 0) { // clone
@@ -175,6 +285,41 @@ long handle_syscall(long sc_no, long arg1, long arg2, long arg3, long arg4,
   } else if (sc_no == SYS_fstat) {
     return ifstat(arg1, (struct stat *)arg2);
   }
+  // Networking
+  else if (sc_no == SYS_socket) {
+    return isocket(arg1, arg2, arg3);
+    // } else if (sc_no == SYS_getsockopt) {
+    //   return igetsockopt(arg1, arg2, arg3, (void *)arg4, (socklen_t *)arg5);
+  } else if (sc_no == SYS_setsockopt) {
+    return isetsockopt(arg1, arg2, arg3, (const void *)arg4, arg5);
+  } else if (sc_no == SYS_bind) {
+    return ibind(arg1, (const struct sockaddr *)arg2, arg3);
+  } else if (sc_no == SYS_listen) {
+    return ilisten(arg1, arg2);
+  } else if (sc_no == SYS_accept) {
+    return iaccept(arg1, (struct sockaddr *)arg2, (socklen_t *)arg3);
+  } else if (sc_no == SYS_accept4) {
+    return iaccept4(arg1, (struct sockaddr *)arg2, (socklen_t *)arg3, arg4);
+  } else if (sc_no == SYS_getsockname) {
+    return igetsockname(arg1, (struct sockaddr *)arg2, (socklen_t *)arg3);
+    // } else if (sc_no == SYS_getpeername) {
+    //   assert(false);
+  } else if (sc_no == SYS_select) {
+    assert(false);
+  } else if (sc_no == SYS_fcntl) {
+    assert(false);
+  } else if (sc_no == SYS_msgsnd) {
+    assert(false);
+  } else if (sc_no == SYS_msgrcv) {
+    assert(false);
+  } else if (sc_no == SYS_sendto) {
+    return isendto(arg1, (const void *)arg2, arg3, arg4,
+                   (struct sockaddr *)arg5, arg6);
+  } else if (sc_no == SYS_recvfrom) {
+    return irecvfrom(arg1, (void *)arg2, arg3, arg4, (struct sockaddr *)arg5,
+                     (socklen_t *)arg6);
+  }
+  // TODO: No forking and threading? Think of FTP server and LIST op.
 
   return real_syscall(sc_no, arg1, arg2, arg3, arg4, arg5, arg6);
 }
@@ -276,4 +421,14 @@ void sbr_init(int *argc, char **argv[], sbr_icept_reg_fn fn_icept_reg,
   ret = zbox_open_repo(&repo, opener, "mem://sabre", "password");
   assert(!ret);
   zbox_free_opener(opener);
+
+  // Sockets
+  int fd[2];
+  int rc = socketpair(AF_LOCAL, SOCK_STREAM, 0, fd);
+  if (rc != 0) {
+    perror("socketpair() failed");
+    exit(EXIT_FAILURE);
+  }
+  sbrsocket = fd[0];
+  childlistensocket = fd[1];
 }
