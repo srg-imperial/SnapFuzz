64627ab0703d25cc291258f6c74b74211b2083ee
Anastasios Andronidis <anastasis90@yahoo.gr>
2020-11-02 02:02:37 +0200

    Net: Implement SaBRe net-fuzzing protocol

--- plugins/sbr-afl/main.c
+++ plugins/sbr-afl/main.c
@@ -168,16 +168,6 @@ ssize_t iwrite(int fd, const void *buf, size_t count) {
   return real_syscall(SYS_write, fd, (long)buf, count, 0, 0, 0);
 }
 
-int iclose(int fd) {
-  if (fd >= fd_offset) {
-    // TODO: We need a mechanism to recycle FDs.
-    sfile_map_seek[fd - fd_offset] = 0;
-    // TODO: Implement a file state and set it to closed.
-    return 0;
-  }
-  return real_syscall(SYS_close, fd, 0, 0, 0, 0, 0);
-}
-
 int ifstat(int fd, struct stat *statbuf) {
   if (fd >= fd_offset) {
     // HINT: Don't use sqlfs_proc_statfs it doesn't support ":memory:".
@@ -227,9 +217,15 @@ int iunlinkat(int dirfd, const char *pathname, int flags) {
   return iunlink(pathname);
 }
 
-static int sbrsocket = -1;
-static int childlistensocket = -1;
+#define AFL_DATA_SOCKET 200
+#define AFL_CTL_SOCKET (AFL_DATA_SOCKET + 1)
+
+typedef enum { Accept, Send, Recv, ExitGroup } SbrState;
+
+static int aflsocket = AFL_DATA_SOCKET;
+// static int childlistensocket = -1;
 static int childacceptsocket = -1;
+static int dbgsbrsocket = -1;
 
 #define CHILD_ACCEPT_PORT 2321
 
@@ -240,7 +236,7 @@ int isocket(int domain, int type, int protocol) {
   assert(type == SOCK_STREAM || type == SOCK_SEQPACKET);
   assert((type & SOCK_NONBLOCK) == 0);
 
-  return childlistensocket;
+  return aflsocket;
 }
 
 // TODO: We should ideally keep track of optval between setsockopt and get.
@@ -261,6 +257,7 @@ int ibind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
 int ilisten(int sockfd, int backlog) { return 0; }
 
 int iaccept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
+  // TODO: we only support 1 accept.
   if (childacceptsocket >= 0) {
     errno = ECONNRESET;
     return -1;
@@ -284,7 +281,13 @@ int iaccept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
     *addrlen = sizeof(peer_addr);
   }
 
-  childacceptsocket = dup(childlistensocket);
+  childacceptsocket = dup(aflsocket);
+
+  // Inform afl that we are ready.
+  SbrState st = Accept;
+  int rc = send(AFL_CTL_SOCKET, &st, sizeof(SbrState), MSG_NOSIGNAL);
+  assert(rc == sizeof(SbrState));
+
   return childacceptsocket;
 }
 
@@ -303,7 +306,7 @@ int igetsockname(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
     copylen = *addrlen;
 
   target.sin_family = AF_INET;
-  target.sin_addr.s_addr = htonl(INADDR_ANY);
+  target.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
   target.sin_port = htons(CHILD_ACCEPT_PORT);
 
   memcpy(addr, &target, copylen);
@@ -312,27 +315,77 @@ int igetsockname(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
   return 0;
 }
 
+int igetpeername(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
+  struct sockaddr_in target;
+  socklen_t copylen = sizeof(target);
+
+  if (!addr || !addrlen)
+    return -1;
+
+  if (*addrlen < sizeof(target))
+    copylen = *addrlen;
+
+  target.sin_family = AF_INET;
+  target.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  target.sin_port = htons(CHILD_ACCEPT_PORT + 100);
+
+  memcpy(addr, &target, copylen);
+  *addrlen = copylen;
+
+  return 0;
+}
+
+int iconnect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+  // dprintf(2, "Trying to connect. We will refuse\n");
+  errno = ECONNREFUSED;
+  return -1;
+}
+
 ssize_t isendto(int sockfd, const void *buf, size_t len, int flags,
                 const struct sockaddr *dest_addr, socklen_t addrlen) {
   assert(sockfd == childacceptsocket);
 
-  ssize_t rc = sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+  SbrState st = Send;
+  int rc = send(AFL_CTL_SOCKET, &st, sizeof(SbrState), MSG_NOSIGNAL);
+  assert(rc == sizeof(SbrState));
 
-  // char rbuf[1024] = {0};
-  // recv(sbrsocket, rbuf, 1024, 0); // sys: recvfrom
-  // printf("sbr: %s", rbuf);
+  // char msg[1024] = {0};
+  // recv(dbgsbrsocket, msg, sizeof(msg), 0); // sys: recvfrom
+  // dprintf(1, "sbr rcv: %s", msg);
 
-  return rc;
+  return sendto(sockfd, buf, len, flags, dest_addr, addrlen);
 }
 
 ssize_t irecvfrom(int sockfd, void *buf, size_t len, int flags,
                   struct sockaddr *src_addr, socklen_t *addrlen) {
   assert(sockfd == childacceptsocket);
 
-  // char msg[] = "lalalalalo\r\n";
-  // send(sbrsocket, msg, sizeof(msg), 0);
+  SbrState st = Recv;
+  int rc = send(AFL_CTL_SOCKET, &st, sizeof(SbrState), MSG_NOSIGNAL);
+  assert(rc == sizeof(SbrState));
 
   return recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
+}
+
+// Close is used in both networking and files.
+int iclose(int fd) {
+  if (fd >= fd_offset) {
+    // TODO: We need a mechanism to recycle FDs.
+    sfile_map_seek[fd - fd_offset] = 0;
+    // TODO: Implement a file state and set it to closed.
+    return 0;
+  }
+  if (fd == AFL_DATA_SOCKET || fd == AFL_CTL_SOCKET) {
+    return 0;
+  }
+  return syscall(SYS_close, fd);
+}
+
+int ishutdown(int sockfd, int how) {
+  if (sockfd == AFL_DATA_SOCKET || sockfd == AFL_CTL_SOCKET) {
+    return 0;
+  }
+  return syscall(SYS_shutdown, sockfd, how);
 }
 
 // Misc
@@ -342,12 +395,33 @@ int inanosleep(const struct timespec *req, struct timespec *rem) {
   return 0;
 }
 
+static bool i_m_forkserver = true;
+
+long iexit_group(int status) {
+  if (i_m_forkserver == false) {
+    SbrState st = ExitGroup;
+    int rc = send(AFL_CTL_SOCKET, &st, sizeof(SbrState), MSG_NOSIGNAL);
+    assert(rc == sizeof(SbrState));
+  }
+
+  return syscall(SYS_exit_group, status);
+}
+
 long handle_syscall(long sc_no, long arg1, long arg2, long arg3, long arg4,
                     long arg5, long arg6, void *wrapper_sp) {
-  if (sc_no == SYS_clone && arg2 != 0) { // clone
-    void *ret_addr = get_syscall_return_address(wrapper_sp);
-    return clone_syscall(arg1, (void *)arg2, (void *)arg3, (void *)arg4, arg5,
-                         ret_addr);
+  if (sc_no == SYS_clone) {
+    if (arg2 != 0) { // clone for threads
+      void *ret_addr = get_syscall_return_address(wrapper_sp);
+      return clone_syscall(arg1, (void *)arg2, (void *)arg3, (void *)arg4, arg5,
+                           ret_addr);
+    } else { // fork -> if (arg2 == 0)
+
+      long rc = real_syscall(sc_no, arg1, arg2, arg3, arg4, arg5, arg6);
+      if (rc == 0) { // We are the afl-forkserver's child
+        i_m_forkserver = false;
+      }
+      return rc;
+    }
   }
 
   // TODO: Switch to a switch
@@ -359,8 +433,6 @@ long handle_syscall(long sc_no, long arg1, long arg2, long arg3, long arg4,
     return iread(arg1, (void *)arg2, arg3);
   } else if (sc_no == SYS_write) {
     return iwrite(arg1, (const void *)arg2, arg3);
-  } else if (sc_no == SYS_close) {
-    return iclose(arg1);
   } else if (sc_no == SYS_fstat) {
     return ifstat(arg1, (struct stat *)arg2);
   } else if (sc_no == SYS_unlink) {
@@ -381,12 +453,21 @@ long handle_syscall(long sc_no, long arg1, long arg2, long arg3, long arg4,
     assert(false);
   } else if (sc_no == SYS_renameat2) {
     assert(false);
-  }
-  // Networking
-  else if (sc_no == SYS_socket) {
+
+    // Networking + FS
+
+  } else if (sc_no == SYS_close) {
+    return iclose(arg1);
+  } else if (sc_no == SYS_shutdown) {
+    return ishutdown(arg1, arg2);
+
+    // Networking
+
+  } else if (sc_no == SYS_socket) {
     return isocket(arg1, arg2, arg3);
-    // } else if (sc_no == SYS_getsockopt) {
-    //   return igetsockopt(arg1, arg2, arg3, (void *)arg4, (socklen_t *)arg5);
+  } else if (sc_no == SYS_getsockopt) {
+    assert(false);
+    // return igetsockopt(arg1, arg2, arg3, (void *)arg4, (socklen_t *)arg5);
   } else if (sc_no == SYS_setsockopt) {
     return isetsockopt(arg1, arg2, arg3, (const void *)arg4, arg5);
   } else if (sc_no == SYS_bind) {
@@ -399,8 +480,8 @@ long handle_syscall(long sc_no, long arg1, long arg2, long arg3, long arg4,
     return iaccept4(arg1, (struct sockaddr *)arg2, (socklen_t *)arg3, arg4);
   } else if (sc_no == SYS_getsockname) {
     return igetsockname(arg1, (struct sockaddr *)arg2, (socklen_t *)arg3);
-    // } else if (sc_no == SYS_getpeername) {
-    //   assert(false);
+  } else if (sc_no == SYS_getpeername) {
+    return igetpeername(arg1, (struct sockaddr *)arg2, (socklen_t *)arg3);
   } else if (sc_no == SYS_select) {
     assert(false);
   } else if (sc_no == SYS_fcntl) {
@@ -409,6 +490,8 @@ long handle_syscall(long sc_no, long arg1, long arg2, long arg3, long arg4,
     assert(false);
   } else if (sc_no == SYS_msgrcv) {
     assert(false);
+  } else if (sc_no == SYS_connect) {
+    return iconnect(arg1, (struct sockaddr *)arg2, arg3);
   } else if (sc_no == SYS_sendto) {
     return isendto(arg1, (const void *)arg2, arg3, arg4,
                    (struct sockaddr *)arg5, arg6);
@@ -419,6 +502,10 @@ long handle_syscall(long sc_no, long arg1, long arg2, long arg3, long arg4,
   // Misc
   else if (sc_no == SYS_nanosleep) {
     return inanosleep((const struct timespec *)arg1, (struct timespec *)arg2);
+  } else if (sc_no == SYS_exit) {
+    /* code */
+  } else if (sc_no == SYS_exit_group) {
+    return iexit_group(arg1);
   }
 
   // TODO: No forking and threading? Think of FTP server and LIST op.
@@ -516,13 +603,40 @@ void sbr_init(int *argc, char **argv[], sbr_icept_reg_fn fn_icept_reg,
 
   // TODO: assert(sqlfs_close(sqlfs));
 
-  // Sockets
-  int fd[2];
-  rc = socketpair(AF_LOCAL, SOCK_STREAM, 0, fd);
-  if (rc != 0) {
-    perror("socketpair() failed");
-    exit(EXIT_FAILURE);
+  struct stat sockstatus;
+  fstat(AFL_CTL_SOCKET, &sockstatus);
+  // If we are under AFL, let's handshake.
+  if (S_ISSOCK(sockstatus.st_mode) == 1) {
+
+    char msg[] = "hello from sbr";
+    rc = send(AFL_CTL_SOCKET, msg, sizeof(msg), MSG_NOSIGNAL);
+    assert(rc == sizeof(msg));
+
+    char rsp[1024] = {0};
+    rc = recv(AFL_CTL_SOCKET, rsp, sizeof(rsp), 0);
+
+    char expected[] = "hello from afl";
+    assert(strncmp(rsp, expected, sizeof(expected)) == 0 &&
+           rc == sizeof(expected));
+  } else {
+    dprintf(2, "WARN: SaBRe-afl is running headless.\n");
+    // TODO: This is broken!
+    // Let's local debug.
+    int sbr_pair[2];
+
+    if (socketpair(AF_LOCAL, SOCK_STREAM, 0, sbr_pair) != 0) {
+      perror("socketpair() failed");
+      exit(EXIT_FAILURE);
+    }
+    if (dup2(sbr_pair[1], AFL_CTL_SOCKET) != AFL_CTL_SOCKET) {
+      perror("dup2() failed");
+      exit(EXIT_FAILURE);
+    }
+    close(sbr_pair[1]);
+
+    dbgsbrsocket = sbr_pair[0];
+
+    fstat(AFL_CTL_SOCKET, &sockstatus);
+    assert(S_ISSOCK(sockstatus.st_mode) == 1);
   }
-  sbrsocket = fd[0];
-  childlistensocket = fd[1];
 }
