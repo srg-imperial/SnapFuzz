b6d33950e623298b6735b735189f1dd2777cd425
Anastasios Andronidis <anastasis90@yahoo.gr>
2021-05-20 12:24:20 +0100

    FS: Targets open /dev/null with O_RDWR

--- plugins/sbr-afl/main.c
+++ plugins/sbr-afl/main.c
@@ -101,7 +101,7 @@ int iopenat(int dirfd, const char *pathname, int flags, mode_t mode) {
   // TODO: What if we read the file multiple times?
 
   if (starts_with(pathname, "/dev/") || starts_with(pathname, "/etc/")) {
-    assert((flags & O_ACCMODE) == O_RDONLY);
+    // assert((flags & O_ACCMODE) == O_RDONLY);
     return real_syscall(SYS_openat, dirfd, (long)pathname, flags, mode, 0, 0);
   }
 
